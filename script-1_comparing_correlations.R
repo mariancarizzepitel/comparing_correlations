3dde5bc6e62723a04f02244a0673bf6a1c331902
Marian Pitel <mpitel@uoguelph.ca>
2016-10-25 11:21:28 -0400

    second commit

--- script-1_comparing_correlations.R
+++ script-1_comparing_correlations.R
@@ -9,6 +9,7 @@ apa.cor.table(bfi)
 ##Are the correlations different? 
 library(cocor)
 
+--
 #To compare correlations based on dependent groups when correlations do not overlap 
 cocor(~A1+C1|E1+O1, data=as.data.frame(bfi))
 #Results
@@ -17,8 +18,29 @@ cocor(~A1+C1|E1+O1, data=as.data.frame(bfi))
 #Difference: r.jk - r.hm = 0.1278 <- this is comparison 
 #Write up: (change in, triangle, delta symbol)r = .13, 95% CI [.08,.18] <- CI from Zou 
 
+--
+  
 #Overlapping correlations - comparing correlations 
 ##e.g., Does agreeableness correlate more with conscientiousness or does it correlate more with E1?
 ##e.g., Does C1 predict job performance (A1) better than E1? 
 cocor(~C1+A1|E1+A1, data=as.data.frame(bfi))
 #write up, (delta)r = -.08, 95% CI [-.13, -.03]
+
+--
+#FILTERING BY GENDER
+bfi_men <- bfi %>% filter(gender==1) %>% select(-gender)
+bfi_women <- bfi %>% filter(gender==2) %>% select(-gender)
+
+#creating correlation table 
+apa.cor.table(bfi_men)
+apa.cor.table(bfi_women)
+##notice r between A1 and E1 for men: .14, women: .06
+
+#create different type of data set/frame, needed or else error will occur
+bfi_men <- as.data.frame(bfi_men)
+bfi_women <- as.data.frame(bfi_women)
+
+#compare now,  ONLY WHEN RAW DATA ACCESSIBLE
+cocor(~A1+E1|A1+E1, data=list(bfi_men, bfi_women))
+-- 
+  
